7bb08f07c0527e4bddcc52f9c9e73270f3f11491
islam soliman <islam.soliman1990@gmail.com>
2023-06-29 11:53:24 +0300

    next 05

--- app/components/Search.tsx
+++ app/components/Search.tsx
@@ -1,29 +1,31 @@
 "use client";
+
 import { useState, FormEvent } from "react";
 import { useRouter } from "next/navigation";
+
 export default function Search() {
-  const [Search, setSearch] = useState(" ");
+  const [search, setSearch] = useState("");
   const router = useRouter();
 
-  const handleSearch = async (e: FormEvent<HTMLFormElement>) => {
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setSearch("");
-    router.push(`/${Search}/`);
+    router.push(`/${search}/`);
   };
 
   return (
     <form
       className="w-50 flex justify-center md:justify-between"
-      onSubmit={handleSearch}
+      onSubmit={handleSubmit}
     >
       <input
         type="text"
-        value={Search}
+        value={search}
         onChange={(e) => setSearch(e.target.value)}
-        className="bg-white p-2 w-80 text-xl rounded-xl border-black "
+        className="bg-white p-2 w-80 text-xl rounded-xl"
         placeholder="Search"
       />
-      <button className="p-2 text-xl rounded-xl bg-dark-300 ml-2 font-bold">
+      <button className="p-2 text-xl rounded-xl bg-slate-300 ml-2 font-bold">
         🚀
       </button>
     </form>
