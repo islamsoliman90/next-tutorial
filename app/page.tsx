ed35471ff6fd01e968cdbe16769bba5ac1c7c44a
islam soliman <islam.soliman1990@gmail.com>
2023-06-29 17:49:00 +0300

    next 06

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
-import { Inter } from 'next/font/google'
-const inter = Inter({ subsets: ['latin'] })
+import Posts from "./components/Posts"
 
 export default function Home() {
   return (
-    <main className={inter.className}>
-
+    <main className="px-6 mx-auto">
+      <p className="mt-12 mb-12 text-3xl text-center dark:text-white">
+        Hello and Welcome 👋&nbsp;
+        <span className="whitespace-nowrap">
+          I&apos;m <span className="font-bold">Dave</span>.
+        </span>
+      </p>
+      <Posts />
     </main>
   )
 }
